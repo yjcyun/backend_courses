22c7ea70873d52162b19178e4eebace35c8890e6
Christina Yujeong Yun <yjc.yun@gmail.com>
2021-05-27 21:15:56 -0400

    9. add delete and edit functionality

--- nodejs-the-complete-guide/7-model-view-controller/routes/admin.js
+++ nodejs-the-complete-guide/7-model-view-controller/routes/admin.js
@@ -3,22 +3,15 @@ const express = require('express');
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
-/**
- * route: '/admin/add-product'
- * request: GET
- */
-router.get('/add-product', adminController.getAddProduct);
 
-/**
- * route: '/admin/add-product'
- * request: POST
- */
+router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
 
-/**
- * route: '/admin/products'
- * request: GET
- */
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.deleteProduct);
+
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
--- nodejs-the-complete-guide/7-model-view-controller/routes/shop.js
+++ nodejs-the-complete-guide/7-model-view-controller/routes/shop.js
@@ -11,6 +11,7 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.get('/checkout', shopController.getCheckout);
 

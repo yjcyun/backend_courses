324cd974319b9e8c4104aa630215a53cd671b1f7
Christina Yujeong Yun <yjc.yun@gmail.com>
2021-05-28 21:10:53 -0400

    10. add and connect to mysql database

---
+++ nodejs-the-complete-guide/10-sql/app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+const app = express();
+
+// SET VIEWS
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
---
+++ nodejs-the-complete-guide/10-sql/models/product.js
@@ -0,0 +1,30 @@
+const db = require('../util/database.js');
+const Cart = require('./cart');
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, price, description) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
+  }
+
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
+
+  static deleteById(id) {}
+
+  // fetch all data
+  static fetchAll() {
+    return db.execute('SELECT * FROM products');
+  }
+
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+  }
+};
